89365135bc45a0ea213c464d9758d29adf0a6c04
lookeastwards <45072211+crawlserv@users.noreply.github.com>
2018-11-16 02:12:15 +0100

    Update ConfigParser.cpp

--- crawlserv/src/ConfigParser.cpp
+++ crawlserv/src/ConfigParser.cpp
@@ -1,7 +1,7 @@
 /*
  * ConfigCrawler.cpp
  *
- * Configuration for a parser.
+ * Parsing configuration.
  *
  * WARNING:	Changing the configuration requires updating 'json/parser.json' in crawlserv_frontend!
  * 			See there for details on the specific configuration entries.
