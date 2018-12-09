d04e94318fa7a160eb0e79893c8fe90eba925e07
Ans <ans@ohai.su>
2018-12-09 19:26:31 +0100

    minor commenting

--- crawlserv/src/namespaces/Helpers.h
+++ crawlserv/src/namespaces/Helpers.h
@@ -51,7 +51,7 @@ namespace Helpers {
 	std::string parseMementos(std::string mementoContent, std::vector<std::string>& warningsTo,
 			std::vector<Memento>& mementosTo);
 
-	// time conversion
+	// fate/time conversions
 	bool convertLongDateToSQLTimeStamp(std::string& timeStamp);
 	bool convertTimeStampToSQLTimeStamp(std::string& timeStamp);
 	bool convertSQLTimeStampToTimeStamp(std::string& timeStamp);
