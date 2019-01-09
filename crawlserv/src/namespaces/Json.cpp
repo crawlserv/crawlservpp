a8561f10749d0575e688ad8dfdf7178bc6486a6d
Ans <ans@ohai.su>
2019-01-09 19:07:34 +0100

    add global JSON helper function in anticipation of parser implementation

---
+++ crawlserv/src/namespaces/Json.cpp
@@ -0,0 +1,35 @@
+/*
+ * Json.cpp
+ *
+ * Namespace with global JSON helper functions.
+ *
+ *  Created on: Jan 9, 2019
+ *      Author: ans
+ */
+
+#include "Json.h"
+
+// stringify a vector of strings to JSON code in one string
+std::string Json::stringify(const std::vector<std::string>& vectorToStringify) {
+	// create document as array and get reference to allocator
+	rapidjson::Document document;
+	document.SetArray();
+	rapidjson::Document::AllocatorType& allocator = document.GetAllocator();
+
+	// write vector elements as string values to array
+	for(auto i = vectorToStringify.begin(); i != vectorToStringify.end(); ++i) {
+		rapidjson::Value stringValue;
+		stringValue.SetString(i->c_str(), i->length(), allocator);
+		document.PushBack(stringValue, allocator);
+	}
+
+	// create string buffer and writer
+	rapidjson::StringBuffer buffer;
+	rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+
+	// write array to string buffer
+	document.Accept(writer);
+
+	// return string
+	return std::string(buffer.GetString(), buffer.GetSize());
+}
---
+++ crawlserv/src/namespaces/Json.h
@@ -0,0 +1,24 @@
+/*
+ * Json.h
+ *
+ * Namespace with global JSON helper functions.
+ *
+ *  Created on: Jan 9, 2019
+ *      Author: ans
+ */
+
+#ifndef SRC_NAMESPACES_JSON_H_
+#define SRC_NAMESPACES_JSON_H_
+
+#include "../external/rapidjson/document.h"
+#include "../external/rapidjson/stringbuffer.h"
+#include "../external/rapidjson/writer.h"
+
+#include <string>
+#include <vector>
+
+namespace Json {
+	std::string stringify(const std::vector<std::string>& vectorToStringify);
+}
+
+#endif /* SRC_NAMESPACES_JSON_H_ */
