2d8c12903aa79cbb7e46d8ecad5ef140029cbe2b
Ans <ans@ohai.su>
2019-01-10 21:24:26 +0100

    wrapper templates for std::min with multiple (more than two) values

---
+++ crawlserv/src/namespaces/Algos.h
@@ -0,0 +1,23 @@
+/*
+ * Algos.h
+ *
+ * Namespace with global helper functions for algorithms.
+ *
+ *  Created on: Jan 10, 2019
+ *      Author: ans
+ */
+
+#ifndef SRC_NAMESPACES_ALGOS_H_
+#define SRC_NAMESPACES_ALGOS_H_
+
+#include <algorithm>
+
+namespace Algos {
+	// return the lowest of the three values
+	template<class T> const T& min(const T& a, const T& b, const T& c) { return std::min(a, std::min(b, c)); }
+
+	// return the lowest of the four values
+	template<class T> const T& min(const T& a, const T& b, const T& c, const T& d) { return std::min(a, std::min(b, std::min(c, d))); }
+}
+
+#endif /* SRC_NAMESPACES_ALGOS_H_ */
