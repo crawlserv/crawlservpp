26959ff5e54b56a06350c42d25a35e92169f9965
Ans <ans@ohai.su>
2019-01-12 21:25:25 +0100

    small fix

--- crawlserv_frontend/crawlserv/php/websites.php
+++ crawlserv_frontend/crawlserv/php/websites.php
@@ -19,7 +19,7 @@ function time_elapsed_string($datetime) {
     );
     foreach ($string as $k => &$v) {
         if ($diff->$k) {
-            $v = $diff->$k . $v . ($diff->$k > 1 ? 's' : '');
+            $v = $diff->$k . $v;
         } else {
             unset($string[$k]);
         }
