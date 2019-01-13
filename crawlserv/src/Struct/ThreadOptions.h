71d328b745bb3a4e2c102b48f79823b676e3b5ea
Ans <ans@ohai.su>
2019-01-13 04:47:36 +0100

    added 'algo' column to thread status table in anticipation of analyzer

--- crawlserv/src/Struct/ThreadOptions.h
+++ crawlserv/src/Struct/ThreadOptions.h
@@ -10,11 +10,22 @@
 #ifndef STRUCT_THREADOPTIONS_H_
 #define STRUCT_THREADOPTIONS_H_
 
+#include "../Module/Analyzer/Algo/List.h"
+
 namespace crawlservpp::Struct {
 	struct ThreadOptions {
 		unsigned long website; // website id for the thread
 		unsigned long urlList; // URL list id for the thread
+		crawlservpp::Module::Analyzer::Algo::List algo; // algorithm for the thread (used by analyzer threads only)
 		unsigned long config; // configuration id for the thread
+
+		// default values
+		ThreadOptions::ThreadOptions() {
+			this->website = 0;
+			this->urlList = 0;
+			this->algo = crawlservpp::Module::Analyzer::Algo::List::none;
+			this->config = 0;
+		}
 	};
 }
 
