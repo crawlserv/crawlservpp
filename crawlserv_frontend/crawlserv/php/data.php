98550513fd1d1ddb8fcce015f634fc81801d41d5
Ans <ans@ohai.su>
2019-01-13 23:10:56 +0100

    marked menu items that will not be implemented at Beta v0.1

--- crawlserv_frontend/crawlserv/php/data.php
+++ crawlserv_frontend/crawlserv/php/data.php
@@ -1,1 +1,3 @@
 <h2>Import/Export</h2>
+
+<p><i>This feature is not implemented yet.</i></p>
--- crawlserv_frontend/crawlserv/php/extractors.php
+++ crawlserv_frontend/crawlserv/php/extractors.php
@@ -1,1 +1,3 @@
-<h2>Extracting</h2>
+<h2>Extracting options</h2>
+
+<p><i>This feature is not implemented yet.</i></p>
