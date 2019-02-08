c23d4b0c1e356ac95c37cdfabaa7778718de0472
Ans <ans@ohai.su>
2019-02-08 02:20:51 +0100

    rule of zero

--- crawlserv/src/Main/Exception.cpp
+++ crawlserv/src/Main/Exception.cpp
@@ -16,17 +16,6 @@ Exception::Exception(const std::string& description) {
 	this->_description = description;
 }
 
-// copy constructor
-Exception::Exception(const Exception& other) {
-	this->_description = other._description;
-}
-
-// move constructor
-Exception::Exception(Exception&& other) noexcept : _description(std::exchange(other._description, nullptr)) {}
-
-// destructor stub
-Exception::~Exception() {}
-
 // get description as C string
 const char * Exception::what() const noexcept {
 	return this->_description.c_str();
@@ -35,17 +24,6 @@ const char * Exception::what() const noexcept {
 // get description as std::string
 const std::string& Exception::whatStr() const noexcept {
 	return this->_description;
-}
-
-// copy assignment operator
-Exception& Exception::operator=(const Exception& other) {
-	return *this = Exception(other);
-}
-
-// move assigmnent operator
-Exception& Exception::operator=(Exception&& other) noexcept {
-	std::swap(this->_description, other._description);
-	return *this;
 }
 
 }
