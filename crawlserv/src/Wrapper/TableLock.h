90339c4fea4a6ef425b38e0161a84cb945460b89
Ans <ans@ohai.su>
2019-03-07 02:57:28 +0100

    added safe in-scope table lock for module threads

---
+++ crawlserv/src/Wrapper/TableLock.h
@@ -0,0 +1,30 @@
+/*
+ * TableLock.h
+ *
+ * Safe in-scope table lock for the module threads.
+ *
+ *  Created on: Mar 7, 2019
+ *      Author: ans
+ */
+
+#ifndef MODULE_TABLELOCK_H_
+#define MODULE_TABLELOCK_H_
+
+#include "Database.h"
+
+namespace crawlservpp::Wrapper {
+
+class TableLock {
+public:
+	TableLock(Database& db, const std::string& tableName);
+	TableLock(Database& db, const std::string& tableName1, const std::string& tableName2);
+	virtual ~TableLock();
+
+private:
+	// internal reference to the database connection of the thread
+	Database& ref;
+};
+
+}
+
+#endif /* MODULE_TABLELOCK_H_ */
