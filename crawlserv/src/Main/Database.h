a062259c6a49c193e3617b52820776da9375d56b
Ans <45072211+crawlserv@users.noreply.github.com>
2019-03-09 19:30:49 +0100

    Update Database.h

--- crawlserv/src/Main/Database.h
+++ crawlserv/src/Main/Database.h
@@ -5,8 +5,7 @@
  * Thread-specific functionality is not implemented in this (parent) class.
  *
  * NOT THREAD-SAFE!
- * Use instances of the child class Module::Database for thread-specific functionality
- * functionality instead.
+ * Use instances of the child class Module::Database for thread-specific functionality functionality instead.
  *
  *  Created on: Sep 29, 2018
  *      Author: ans
