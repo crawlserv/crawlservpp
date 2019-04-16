60f858498598901f0c7b33c0e89a869be86a05cc
Ans <ans@ohai.su>
2019-04-16 21:23:31 +0300

    fixed code snippet on XML parsing error

--- crawlserv/src/Parsing/XML.cpp
+++ crawlserv/src/Parsing/XML.cpp
@@ -21,12 +21,16 @@ namespace crawlservpp::Parsing {
 	void XML::parse(const std::string& content) {
 		// remove whitespaces
 		unsigned long begin = 0;
-		while(content.size() > begin && isspace(content.at(begin))) ++begin;
+
+		while(content.size() > begin && isspace(content.at(begin)))
+			++begin;
+
 		std::string xml(content, begin, content.length() - begin);
 
 		// if necessary, try to tidy HTML and convert it to XML
 		if(xml.size() <= 6 || xml.substr(0, 6) != "<?xml ") {
 			HTML tidy;
+
 			try {
 				tidy.tidyAndConvert(xml);
 			}
@@ -40,16 +44,33 @@ namespace crawlservpp::Parsing {
 
 		// parse XHTML with pugixml
 		std::istringstream in(xml);
+
 		pugi::xml_parse_result result = this->doc->load(in, pugi::parse_full);
 
 		if(!result) {
 			// parsing error
 			std::ostringstream errStrStr;
+
 			errStrStr << "XML parsing error: " << result.description() << " at #" << result.offset << " (";
-			if((long) content.size() > result.offset + 100) errStrStr << "\'[...]" << content.substr(result.offset, 100) << "[...]\'";
-			else if((long) content.size() > result.offset) errStrStr << "\'[...]" << content.substr(result.offset);
-			else errStrStr << "[end]";
-			errStrStr << ").";
+
+			if(result.offset > 0) {
+				errStrStr << "\'[...]";
+
+				if(result.offset > 50)
+					errStrStr << xml.substr(result.offset - 50, 50);
+				else
+					errStrStr << xml.substr(0, result.offset);
+
+				errStrStr << "[!!!]";
+
+				if(xml.size() > static_cast<size_t>(result.offset + 50))
+					errStrStr << "\'[...]" << xml.substr(result.offset, 50) << "[...]";
+				else
+					errStrStr << "\'[...]" << xml.substr(result.offset);
+
+				errStrStr << "\').";
+			}
+
 			throw XML::Exception(errStrStr.str());
 		}
 	}
