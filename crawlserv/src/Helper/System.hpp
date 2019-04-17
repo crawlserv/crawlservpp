3fa56c4bcc96e82c640bfd1da816a1b3af23a228
Ans <ans@ohai.su>
2019-04-17 16:03:45 +0300

    run system commands and get their result

---
+++ crawlserv/src/Helper/System.hpp
@@ -0,0 +1,32 @@
+/*
+ * System.hpp
+ *
+ * Namespace with global system helper function.
+ *
+ *  Created on: Apr 17, 2019
+ *      Author: ans
+ */
+
+#include <cstdio>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <array>
+
+namespace crawlservpp::Helper::System {
+	// execute a system command and return the stdout of the program
+	std::string exec(const char* cmd) {
+	    std::array<char, 128> buffer;
+	    std::string result;
+	    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+
+	    if(!pipe)
+	        throw std::runtime_error("popen() failed");
+
+	    while(fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
+	        result += buffer.data();
+
+	    return result;
+	}
+}
