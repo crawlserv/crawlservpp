556d5cb2b39bf1a930fbd0d40dddb141f3cd96b9
Ans <ans@ohai.su>
2019-04-29 23:58:29 +0300

    fixed crawling update time, coding style

--- crawlserv_frontend/crawlserv/php/websites.php
+++ crawlserv_frontend/crawlserv/php/websites.php
@@ -3,6 +3,7 @@
 function time_elapsed_string($datetime) {
     $now = new DateTime;
     $ago = new DateTime($datetime);
+    
     $diff = $now->diff($ago);
     
     $diff->w = floor($diff->d / 7);
@@ -17,20 +18,24 @@ function time_elapsed_string($datetime) {
         'i' => 'min',
         's' => 's',
     );
-    foreach ($string as $k => &$v) {
-        if ($diff->$k) {
+    
+    foreach($string as $k => &$v) {
+        if ($diff->$k)
             $v = $diff->$k . $v;
-        } else {
+        else
             unset($string[$k]);
-        }
     }
     
     $string = array_slice($string, 0, 1);
+    
     return $string ? implode(', ', $string) . ' ago' : 'just now';
 }
 
-if(isset($_POST["website"])) $website = $_POST["website"];
-if(isset($_POST["urllist"])) $urllist = $_POST["urllist"];
+if(isset($_POST["website"]))
+    $website = $_POST["website"];
+
+if(isset($_POST["urllist"]))
+    $urllist = $_POST["urllist"];
 ?>
 <h2>Websites</h2>
 <div class="content-block">
@@ -39,41 +44,65 @@ if(isset($_POST["urllist"])) $urllist = $_POST["urllist"];
 <select class="entry-x-input" id="website-select" data-m="websites">
 <?php
 $result = $dbConnection->query("SELECT id,name FROM crawlserv_websites ORDER BY name");
-if(!$result) exit("ERROR: Could not get ids and names of websites.");
+
+if(!$result)
+    exit("ERROR: Could not get ids and names of websites.");
+
 $first = true;
+
 while($row = $result->fetch_assoc()) {
     $id = $row["id"];
     $name = $row["name"];
     
     if($first) {
-        if(!isset($website)) $website = $id;
+        if(!isset($website))
+            $website = $id;
+        
         $first = false;
     }
+    
     echo "<option value=\"".$id."\"";
+    
     if($website == $id) {
         echo " selected";
+        
         $websiteName = $name;
     }
+    
     echo ">".htmlspecialchars($name)."</option>\n";
 }
+
 $result->close();
 
 if(isset($website)) {
     if($website) {
         // get namespace and domain of website
-        $result = $dbConnection->query("SELECT namespace,domain,dir FROM crawlserv_websites WHERE id=$website LIMIT 1");
-        if(!$result) exit("ERROR: Could not get namespace and domain of website.");
+        $result = $dbConnection->query(
+                "SELECT namespace,domain,dir FROM crawlserv_websites WHERE id=$website LIMIT 1"
+        );
+        
+        if(!$result)
+            exit("ERROR: Could not get namespace and domain of website.");
+        
         $row = $result->fetch_assoc();
-        if(!$row) exit("ERROR: Could not get namespace and domain of website.");
+        
+        if(!$row)
+            exit("ERROR: Could not get namespace and domain of website.");
+        
         $namespace = $row["namespace"];
         $domain = $row["domain"];
         $dir = $row["dir"];
+        
         $result->close();
     }
 }
-else $website = 0;
+else
+    $website = 0;
 ?>
-<option value="0"<?php if(!$website) echo " selected"; ?>>Add new</option>
+<option value="0"<?php
+if(!$website)
+    echo " selected";
+?>>Add new</option>
 </select>
 <a href="#" class="actionlink website-delete"><span class="remove-entry">X</span></a>
 </div>
@@ -81,7 +110,8 @@ else $website = 0;
 <div class="action-link-box">
 <div class="action-link">
 <?php
-if($website) echo "<a href=\"#\" class=\"action-link website-duplicate\">Duplicate configuration</a>";
+if($website)
+    echo "<a href=\"#\" class=\"action-link website-duplicate\">Duplicate configuration</a>";
 ?>
 </div>
 </div>
@@ -89,30 +119,43 @@ if($website) echo "<a href=\"#\" class=\"action-link website-duplicate\">Duplica
 <div class="content-block">
 <div class="entry-row">
 <div class="entry-label">Name:</div><div class="entry-input">
-<input type="text" class="entry-input" id="website-name" value="<?php if($website) echo htmlspecialchars($websiteName); ?>" />
+<input type="text" class="entry-input" id="website-name" value="<?php
+if($website)
+    echo htmlspecialchars($websiteName);
+?>" />
 </div>
 </div>
 <div class="entry-row">
 <div class="entry-label">Namespace:</div><div class="entry-input">
-<input type="text" class="entry-input" id="website-namespace" value="<?php if($website) echo htmlspecialchars($namespace); ?>" />
+<input type="text" class="entry-input" id="website-namespace" value="<?php
+if($website)
+    echo htmlspecialchars($namespace);
+?>" />
 </div>
 </div>
 <div class="entry-row">
 <div class="entry-label">Domain:</div><div class="entry-input">
-<input type="text" class="entry-input" id="website-domain" value="<?php if($website) echo htmlspecialchars($domain); ?>" />
+<input type="text" class="entry-input" id="website-domain" value="<?php
+if($website)
+    echo htmlspecialchars($domain);
+?>" />
 </div>
 </div>
 <div class="entry-row">
 <div class="entry-label"></div>
 <div class="entry-input">
-<input type="checkbox" id="website-crossdomain" <?php if($website && !strlen($domain)) echo "checked"; ?> /> cross-domain website
+<input type="checkbox" id="website-crossdomain" <?php
+if($website && !strlen($domain))
+    echo "checked";
+?> /> cross-domain website
 </div>
 </div>
 <div class="entry-row">
 <div class="entry-label"></div>
 <div class="entry-input">
 <?php
-if(!$website) echo '<input type="checkbox" id="website-externaldir" /> use external directory';
+if(!$website)
+    echo '<input type="checkbox" id="website-externaldir" /> use external directory';
 ?>
 </div>
 </div>
@@ -120,16 +163,22 @@ if(!$website) echo '<input type="checkbox" id="website-externaldir" /> use exter
 <div class="entry-label">Directory:</div>
 <div class="entry-input">
 <input type="text" class="entry-input" <?php
-if($website) echo 'disabled';
-else echo 'id="website-dir"';
-?> value="<?php echo $website && strlen($dir) ? htmlspecialchars($dir) : "[default]"; ?>" />
+if($website)
+    echo 'disabled';
+else
+    echo 'id="website-dir"';
+?> value="<?php
+echo $website && strlen($dir) ? htmlspecialchars($dir) : "[default]";
+?>" />
 </div>
 </div>
 <div class="action-link-box">
 <div class="action-link">
 <?php
-if($website) echo "<a href=\"#\" class=\"action-link website-update\">Change website</a>";
-else echo "<a href=\"#\" class=\"action-link website-add\">Add website</a>";
+if($website)
+    echo "<a href=\"#\" class=\"action-link website-update\">Change website</a>";
+else
+    echo "<a href=\"#\" class=\"action-link website-add\">Add website</a>";
 ?>
 </div>
 </div>
@@ -137,12 +186,18 @@ else echo "<a href=\"#\" class=\"action-link website-add\">Add website</a>";
 <?php 
 if($website) {
     flush();
+    
     echo "<div class=\"content-block\">\n";
     echo "<div class=\"entry-label\">URL list:</div><div class=\"entry-x-input\">\n";
     echo "<select class=\"entry-x-input\" id=\"urllist-select\" data-m=\"$m\">\n";
     
-    $result = $dbConnection->query("SELECT id,name,namespace FROM crawlserv_urllists WHERE website=$website ORDER BY name");
-    if(!$result) exit("ERROR: Could not get URL lists of website.");
+    $result = $dbConnection->query(
+            "SELECT id,name,namespace FROM crawlserv_urllists WHERE website=$website ORDER BY name"
+    );
+    
+    if(!$result)
+        exit("ERROR: Could not get URL lists of website.");
+    
     if($result) {
         $first = true;
         while($row = $result->fetch_assoc()) {
@@ -167,56 +222,106 @@ if($website) {
     
     if(isset($urllist) && $urllist) {
         // get size of selected URL list
-        $result = $dbConnection->query("SELECT COUNT(*) FROM `crawlserv_".$namespace."_".$urllistNamespace."`");
-        if(!$result) exit("ERROR: Could not get size of URL list from `crawlserv_".$namespace."_".$urllistNamespace."`");
+        $result = $dbConnection->query(
+                "SELECT COUNT(*) FROM `crawlserv_".$namespace."_".$urllistNamespace."`"
+        );
+        
+        if(!$result)
+            exit(
+                "ERROR: Could not get size of URL list from `crawlserv_".$namespace."_".$urllistNamespace."`"
+            );
+        
         $row = $result->fetch_row();
-        if($row) $urllistSize = $row[0];
+        
+        if($row)
+            $urllistSize = $row[0];
+        
         $result->close();
         
         // get number of crawled URLs
-        $result = $dbConnection->query("SELECT COUNT(DISTINCT url) FROM `crawlserv_".$namespace."_".$urllistNamespace
-                ."_crawling` WHERE success = TRUE");
-        if(!$result) exit("ERROR: Could not get number of crawled URLs from `crawlserv_".$namespace."_".$urllistNamespace."_crawling`");
+        $result = $dbConnection->query(
+                "SELECT COUNT(DISTINCT url) FROM `crawlserv_".$namespace."_".$urllistNamespace
+                ."_crawling` WHERE success = TRUE"
+        );
+        
+        if(!$result)
+            exit("ERROR: Could not get number of crawled URLs from `crawlserv_".$namespace."_".$urllistNamespace."_crawling`");
+        
         $row = $result->fetch_row();
-        if($row) $urllistCrawled = $row[0];
+        
+        if($row)
+            $urllistCrawled = $row[0];
+        
         $result->close();
         
         // get number of parsed URLs
-        $result = $dbConnection->query("SELECT COUNT(DISTINCT url) FROM `crawlserv_".$namespace."_".$urllistNamespace
-                ."_parsing` WHERE success = TRUE");
-        if(!$result) exit("ERROR: Could not get number of parsed URLs from `crawlserv_".$namespace."_".$urllistNamespace."_parsing`");
+        $result = $dbConnection->query(
+                "SELECT COUNT(DISTINCT url) FROM `crawlserv_".$namespace."_".$urllistNamespace
+                ."_parsing` WHERE success = TRUE"
+        );
+        
+        if(!$result)
+            exit("ERROR: Could not get number of parsed URLs from `crawlserv_".$namespace."_".$urllistNamespace."_parsing`");
+        
         $row = $result->fetch_row();
-        if($row) $urllistParsed = $row[0];
+        
+        if($row)
+            $urllistParsed = $row[0];
+        
         $result->close();
         
         // get number of extracted URLs
-        $result = $dbConnection->query("SELECT COUNT(DISTINCT url) FROM `crawlserv_".$namespace."_".$urllistNamespace
-                ."_extracting` WHERE success = TRUE");
-        if(!$result) exit("ERROR: Could not get number of extracted URLs from `crawlserv_".$namespace."_".$urllistNamespace."_extracting`");
+        $result = $dbConnection->query(
+                "SELECT COUNT(DISTINCT url) FROM `crawlserv_".$namespace."_".$urllistNamespace
+                ."_extracting` WHERE success = TRUE"
+        );
+        
+        if(!$result)
+            exit(
+                "ERROR: Could not get number of extracted URLs from `crawlserv_".$namespace."_".$urllistNamespace."_extracting`"
+            );
+        
         $row = $result->fetch_row();
-        if($row) $urllistExtracted = $row[0];
+        
+        if($row)
+            $urllistExtracted = $row[0];
+        
         $result->close();
         
         // get last update of selected URL list
-        $result = $dbConnection->query("SELECT UPDATE_TIME FROM information_schema.tables WHERE TABLE_SCHEMA='".$db_name."'"
-           ." AND TABLE_NAME='crawlserv_".$namespace."_".$urllistNamespace."'");
+        $result = $dbConnection->query(
+                "SELECT MAX(crawltime) AS updated FROM `crawlserv_".$namespace."_".$urllistNamespace."_crawled`"
+        );
+        
         if($result) {
             $row = $result->fetch_assoc();
-            if($row) $urllistUpdate = $row["UPDATE_TIME"];
+            
+            if($row)
+                $urllistUpdate = $row["updated"];
         }
         
         // get last update of any parsing table
-        $result = $dbConnection->query("SELECT updated FROM crawlserv_parsedtables WHERE urllist=$ulId ORDER BY updated DESC LIMIT 1");
+        $result = $dbConnection->query(
+                "SELECT updated FROM crawlserv_parsedtables WHERE website=$website AND urllist=$ulId ORDER BY updated DESC LIMIT 1"
+        );
+        
         if($result) {
             $row = $result->fetch_assoc();
-            if($row) $parsedUpdate = $row["updated"];
+            
+            if($row)
+                $parsedUpdate = $row["updated"];
         }
         
         // get last update of any extracting table
-        $result = $dbConnection->query("SELECT updated FROM crawlserv_extractedtables WHERE urllist=$ulId ORDER BY updated  DESC LIMIT 1");
+        $result = $dbConnection->query(
+                "SELECT updated FROM crawlserv_extractedtables WHERE website=$website AND urllist=$ulId ORDER BY updated  DESC LIMIT 1"
+        );
+        
         if($result) {
             $row = $result->fetch_assoc();
-            if($row) $extractedUpdate = $row["updated"];
+            
+            if($row)
+                $extractedUpdate = $row["updated"];
         }
     }
     else {
@@ -225,90 +330,140 @@ if($website) {
         $urllistNamespace = "";
     }
     
-    if(!$urllist) echo "<option value=\"0\" selected>Add new</option>\n";
-    else echo "<option value=\"0\">Add new</option>\n";
+    if(!$urllist)
+        echo "<option value=\"0\" selected>Add new</option>\n";
+    else
+        echo "<option value=\"0\">Add new</option>\n";
+    
     echo "</select>\n";
     echo "<a href=\"#\" class=\"actionlink urllist-delete\" ><span class=\"remove-entry\">X</span></a>\n";
     echo "</div>\n</div>\n";
     echo "<div class=\"content-block\">\n";
     echo "<div class=\"entry-row\">\n";
     echo "<div class=\"entry-label\">Name:</div><div class=\"entry-input\">\n";
-    echo "<input type=\"text\" class=\"entry-input\" id=\"urllist-name\" value=\"".htmlspecialchars($urllistName)."\" /></div>\n";
+    echo "<input type=\"text\" class=\"entry-input\" id=\"urllist-name\" value=\"";
+    
+    echo htmlspecialchars($urllistName);
+    
+    echo "\" /></div>\n";
     echo "</div>\n";
     echo "<div class=\"entry-row\">\n";
     echo "<div class=\"entry-label\">Namespace:</div><div class=\"entry-input\">\n";
-    echo "<input type=\"text\" class=\"entry-input\" id=\"urllist-namespace\" value=\"".htmlspecialchars($urllistNamespace)."\" /></div>\n";
+    echo "<input type=\"text\" class=\"entry-input\" id=\"urllist-namespace\" value=\"";
+    
+    echo htmlspecialchars($urllistNamespace);
+    
+    echo "\" /></div>\n";
     echo "</div>\n";
+    
     if($urllist) {
         echo "<div class=\"entry-row\">\n";
         echo "<div class=\"entry-label\">Size:</div>\n";
         echo "<div class=\"entry-value\">\n";
-        if($urllistSize == 1) echo "1 entry";
-        else echo number_format($urllistSize)." entries";
+        
+        if($urllistSize == 1)
+            echo "1 entry";
+        else
+            echo number_format($urllistSize)." entries";
+        
         echo " <a href=\"#\" class=\"urllist-download entry-value\" target=\"_blank\"";
-        echo " data-website-namespace=\"".htmlspecialchars($namespace)."\" data-namespace=\"".htmlspecialchars($urllistNamespace)
-                ."\">[Download]</a>\n";
+        echo " data-website-namespace=\"";
+        
+        echo htmlspecialchars($namespace);
+        
+        echo "\" data-namespace=\"";
+        
+        echo htmlspecialchars($urllistNamespace);
+        
+        echo "\">[Download]</a>\n";
         echo "</div>\n";
         echo "</div>\n";
+        
         if($urllistCrawled) {
             echo "<div class=\"entry-row\">\n";
             echo "<div class=\"entry-label\">Crawled:</div>\n";
             echo "<div class=\"entry-value\">\n";
-            if($urllistCrawled == 1) echo "1 entry";
-            else echo number_format($urllistCrawled)." entries";
+            
+            if($urllistCrawled == 1)
+                echo "1 entry";
+            else
+                echo number_format($urllistCrawled)." entries";
+            
             echo " (";
             echo number_format((float) $urllistCrawled / $urllistSize * 100, 1);
             echo "%";
+            
             if(isset($urllistUpdate) && $urllistUpdate) {
                 echo ", ";
                 echo time_elapsed_string($urllistUpdate);
             }
+            
             echo ")";
             echo "</div>\n";
             echo "</div>\n";
         }
+        
         if($urllistParsed) {
             echo "<div class=\"entry-row\">\n";
             echo "<div class=\"entry-label\">Parsed:</div>\n";
             echo "<div class=\"entry-value\">\n";
-            if($urllistParsed == 1) echo "1 entry";
-            else echo number_format($urllistParsed)." entries";
+            
+            if($urllistParsed == 1)
+                echo "1 entry";
+            else
+                echo number_format($urllistParsed)." entries";
+            
             echo " (";
+            
             echo number_format((float) $urllistParsed / $urllistSize * 100, 1);
+            
             echo "%";
+            
             if(isset($parsedUpdate) && $parsedUpdate) {
                 echo ", ";
                 echo time_elapsed_string($parsedUpdate);
             }
+            
             echo ") <a href=\"#\" class=\"urllist-reset-parsing entry-value\">[Reset]</a>";
             echo "</div>\n";
             echo "</div>\n";
         }
+        
         if($urllistExtracted) {
             echo "<div class=\"entry-row\">\n";
             echo "<div class=\"entry-label\">Extracted:</div>\n";
             echo "<div class=\"entry-value\">\n";
-            if($urllistExtracted == 1) echo "1 entry";
-            else echo number_format($urllistExtracted)." entries";
+            
+            if($urllistExtracted == 1)
+                echo "1 entry";
+            else
+                echo number_format($urllistExtracted)." entries";
+            
             echo " (";
             echo number_format((float) $urllistExtracted / $urllistParsed * 100, 1);
             echo "%";
+            
             if(isset($extractedUpdate) && $extractedUpdate) {
                 echo ", ";
                 echo time_elapsed_string($extractedUpdate);
             }
+            
             echo ") <a href=\"#\" class=\"urllist-reset-extracting entry-value\">[Reset]</a>";
             echo "</div>\n";
             echo "</div>\n";
         }
     }
+    
     echo "<div class=\"action-link-box\">\n";
     echo "<div class=\"action-link\">\n";
+    
     if($urllist) {
         echo "<a href=\"#\" class=\"action-link urllist-update\">";
         echo "Change URL list</a>\n";
     }
-    else echo "<a href=\"#\" class=\"action-link urllist-add\">Add URL list</a>\n";
+    else
+        echo "<a href=\"#\" class=\"action-link urllist-add\">Add URL list</a>\n";
+    
     echo "</div>\n";
     echo "</div>\n";
     echo "</div>\n";
