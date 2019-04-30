3e31f4370c36d9e691d43ba806373a719e7d2396
Ans <ans@ohai.su>
2019-04-30 18:08:53 +0300

    file uplodad

--- crawlserv/src/Main/WebServer.cpp
+++ crawlserv/src/Main/WebServer.cpp
@@ -12,7 +12,7 @@
 namespace crawlservpp::Main {
 
 	// constructor: set default value
-	WebServer::WebServer() {
+	WebServer::WebServer(const std::string& fileCacheDirectory) : fileCache(fileCacheDirectory) {
 		// initialize embedded web server
 		mg_mgr_init(&(this->eventManager), nullptr);
 	}
@@ -25,8 +25,14 @@ namespace crawlservpp::Main {
 	// initialize embedded web server and bind it to port, throws std::runtime_error
 	void WebServer::initHTTP(const std::string& port) {
 		// bind mongoose server to port
-		ConnectionPtr connection = mg_bind(&(this->eventManager), port.c_str(), WebServer::eventHandler);
-		if(!connection) throw std::runtime_error("Could not bind server to port " + port);
+		ConnectionPtr connection = mg_bind(
+				&(this->eventManager),
+				port.c_str(),
+				WebServer::eventHandler
+		);
+
+		if(!connection)
+			throw std::runtime_error("Could not bind server to port " + port);
 
 		// set user data (i.e. pointer to class)
 		connection->user_data = this;
@@ -57,7 +63,12 @@ namespace crawlservpp::Main {
 	}
 
 	// send reply, throws std::runtime_error
-	void WebServer::send(ConnectionPtr connection, unsigned short code, const std::string& type, const std::string& content) {
+	void WebServer::send(
+			ConnectionPtr connection,
+			unsigned short code,
+			const std::string& type,
+			const std::string& content
+	) {
 		// check connection
 		if(!connection)
 			throw std::runtime_error("WebServer::send(): No connection specified");
@@ -77,47 +88,180 @@ namespace crawlservpp::Main {
 		mg_printf(connection, "%s", content.c_str());
 	}
 
+	// send error (HTTP code 500) with custom error message and close connection, throws std::runtime_error
+	void WebServer::sendError(ConnectionPtr connection, const std::string& error) {
+		// check for connection
+		if(!connection)
+			throw std::runtime_error("WebServer::sendError(): No connection specified");
+
+		std::string responseStr =
+				"HTTP/1.1 500 "
+				+ error + "\r\n"
+				"Content-Length: 0\r\n\r\n";
+
+		mg_printf(
+				connection,
+				"%s",
+				responseStr.c_str()
+		);
+
+		connection->flags |= MG_F_SEND_AND_CLOSE;
+	}
+
 	// close connection immediately, throws std::runtime_error
 	void WebServer::close(ConnectionPtr connection) {
 		// check for connection
-		if(!connection) throw std::runtime_error("WebServer::close(): No connection specified");
+		if(!connection)
+			throw std::runtime_error("WebServer::close(): No connection specified");
 
 		// set closing flag
 		connection->flags |= MG_F_CLOSE_IMMEDIATELY;
 	}
 
+	// static helper function: get client IP from connection
+	std::string WebServer::getIP(ConnectionPtr connection) {
+		char ip[INET6_ADDRSTRLEN];
+
+		mg_sock_to_str(
+				connection->sock,
+				ip,
+				INET6_ADDRSTRLEN,
+				MG_SOCK_STRINGIFY_REMOTE | MG_SOCK_STRINGIFY_IP
+		);
+
+		return std::string(ip);
+	}
+
 	// static event handler
 	void WebServer::eventHandler(ConnectionPtr connection, int event, void * data) {
 		if(connection->user_data && data)
-			static_cast<WebServer *>(connection->user_data)->eventHandlerInClass(connection, event, data);
+			static_cast<WebServer *>(connection->user_data)->eventHandlerInClass(
+					connection,
+					event,
+					data
+			);
 	}
 
 	// event handler
 	void WebServer::eventHandlerInClass(ConnectionPtr connection, int event, void * data) {
-		// get ip
-		std::string ip = WebServer::getIP(connection);
+		http_message * httpMessage = nullptr;
+
+		std::string method;
+		std::string body;
+		std::string ip = WebServer::getIP(connection); // get IP
+		std::string fileName;
+
+		WebServer::FileUploadData * uploadData = nullptr;
+		struct mg_http_multipart_part * multiPart = nullptr;
 
 		switch(event) {
 		case MG_EV_ACCEPT:
 			// handle accept event
 			this->onAccept(connection);
+
 			break;
 
 		case MG_EV_HTTP_REQUEST:
 			// handle request event
-			http_message * httpMessage = (http_message *) data;
-			std::string method(httpMessage->method.p, httpMessage->method.len);
-			std::string body(httpMessage->body.p, httpMessage->body.len);
+			httpMessage = (http_message *) data;
+			method = std::string(httpMessage->method.p, httpMessage->method.len);
+			body = std::string(httpMessage->body.p, httpMessage->body.len);
+
 			this->onRequest(connection, method, body);
+
+			break;
+
+		case MG_EV_HTTP_PART_BEGIN:
+			// handle begin of file upload: check whether writer already exists
+			if(connection->user_data)
+				break;
+
+			// generate file name
+			do {
+				fileName = Helper::Strings::generateRandom(128);
+			}
+			while(
+					Helper::FileSystem::exists(
+							this->fileCache
+							+ Helper::FileSystem::getPathSeparator()
+							+ fileName
+					)
+			);
+
+			// create upload data and open file for writing
+			uploadData = new WebServer::FileUploadData(
+					fileName,
+					this->fileCache
+					+ Helper::FileSystem::getPathSeparator()
+					+ fileName
+			);
+
+			// check whether file has been opened
+			if(!(uploadData->stream.is_open())) {
+				WebServer::sendError(
+						connection,
+						"Failed to open \'"
+						+ fileName
+						+ "\' for reading"
+				);
+
+				break;
+			}
+
+			// save upload data
+			connection->user_data = static_cast<void *>(uploadData);
+
+			break;
+
+		case MG_EV_HTTP_PART_DATA:
+			// handle part of file upload
+			if(connection->user_data) {
+				uploadData = static_cast<WebServer::FileUploadData *>(connection->user_data);
+				multiPart = static_cast<struct mg_http_multipart_part *>(data);
+
+				if(multiPart && multiPart->data.p && multiPart->data.len) {
+					uploadData->stream << std::string(multiPart->data.p, multiPart->data.len);
+					uploadData->stream << std::flush;
+				}
+			}
+			else
+				WebServer::sendError(connection, "No file open to write to");
+
+			break;
+
+		case MG_EV_HTTP_PART_END:
+			// handle end of file upload
+			if(connection->user_data) {
+				uploadData = static_cast<WebServer::FileUploadData *>(connection->user_data);
+
+				if(uploadData->stream.is_open())
+					uploadData->stream.close();
+
+				this->send(connection, 200, "text/plain", uploadData->name);
+
+				delete uploadData;
+
+				connection->user_data = nullptr;
+			}
+			else
+				WebServer::sendError(connection, "Missing information about file");
+
+			break;
+
+		case MG_EV_CLOSE:
+			// handle closing of connection
+			if(connection->user_data) {
+				uploadData = static_cast<WebServer::FileUploadData *>(connection->user_data);
+
+				if(uploadData->stream.is_open())
+					uploadData->stream.close();
+
+				delete uploadData;
+
+				connection->user_data = nullptr;
+			}
+
 			break;
 		}
 	}
-
-	// static helper function: get client IP from connection
-	std::string WebServer::getIP(ConnectionPtr connection) {
-		char ip[INET6_ADDRSTRLEN];
-		mg_sock_to_str(connection->sock, ip, INET6_ADDRSTRLEN, MG_SOCK_STRINGIFY_REMOTE | MG_SOCK_STRINGIFY_IP);
-		return std::string(ip);
-	}
-
 } /* crawlservpp::Main */
--- crawlserv/src/Main/WebServer.hpp
+++ crawlserv/src/Main/WebServer.hpp
@@ -13,8 +13,14 @@
 #ifndef MAIN_WEBSERVER_HPP_
 #define MAIN_WEBSERVER_HPP_
 
+#include "../Helper/FileSystem.hpp"
+#include "../Helper/Strings.hpp"
+
+#define MG_ENABLE_HTTP_STREAMING_MULTIPART 1
+
 #include "../_extern/mongoose/mongoose.h"
 
+#include <fstream>
 #include <functional>
 #include <memory>
 #include <stdexcept>
@@ -30,7 +36,7 @@ namespace crawlservpp::Main {
 		typedef std::function<void(ConnectionPtr, const std::string&, const std::string&)> RequestCallback;
 
 	public:
-		WebServer();
+		WebServer(const std::string& fileCacheDirectory);
 		virtual ~WebServer();
 
 		// initializer
@@ -43,8 +49,17 @@ namespace crawlservpp::Main {
 
 		// server functions
 		void poll(int timeOut);
-		void send(ConnectionPtr connection, unsigned short code, const std::string& type, const std::string& content);
-		void close(ConnectionPtr connection);
+		void send(
+				ConnectionPtr connection,
+				unsigned short code,
+				const std::string& type,
+				const std::string& content
+		);
+		static void sendError(
+				ConnectionPtr connection,
+				const std::string& error
+		);
+		static void close(ConnectionPtr connection);
 
 		// static helper function
 		static std::string getIP(ConnectionPtr connection);
@@ -56,6 +71,8 @@ namespace crawlservpp::Main {
 		WebServer& operator=(WebServer&&) = delete;
 
 	private:
+		const std::string fileCache;
+
 		mg_mgr eventManager;
 		std::string cors;
 
@@ -66,6 +83,16 @@ namespace crawlservpp::Main {
 		// event handler
 		static void eventHandler(ConnectionPtr connection, int event, void * data);
 		void eventHandlerInClass(ConnectionPtr connection, int event, void * data);
+
+		// struct for user data while uploading file
+		struct FileUploadData {
+			std::string name;
+			std::ofstream stream;
+
+			FileUploadData(const std::string& fileName, const std::string& fullName) : name(fileName) {
+				this->stream.open(fullName.c_str(), std::ios_base::binary);
+			}
+		};
 	};
 
 } /* crawlservpp::Main */
