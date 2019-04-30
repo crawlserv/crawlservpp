d4712fceda4f01f5b7e119296f4d2f8c28fffab8
Ans <ans@ohai.su>
2019-04-30 13:32:57 +0300

    rework of ThreadOptions

--- crawlserv/src/Struct/ThreadOptions.hpp
+++ crawlserv/src/Struct/ThreadOptions.hpp
@@ -10,26 +10,40 @@
 #ifndef STRUCT_THREADOPTIONS_HPP_
 #define STRUCT_THREADOPTIONS_HPP_
 
+#include <string>
+
 namespace crawlservpp::Struct {
 
 	struct ThreadOptions {
-		unsigned long website; // website id for the thread
-		unsigned long urlList; // URL list id for the thread
-		unsigned long config; // configuration id for the thread
+		std::string module; // module of the thread
+
+		unsigned long website; // website ID for the thread
+		unsigned long urlList; // URL list ID for the thread
+		unsigned long config; // configuration ID for the thread
 
 		// constructors
-		ThreadOptions()
+		ThreadOptions() // create empty thread options
 				: website(0),
 				  urlList(0),
 				  config(0) {}
 
-		ThreadOptions(
+		ThreadOptions( // create thread options
+				const std::string& setModule,
 				unsigned long setWebsite,
 				unsigned long setUrlList,
 				unsigned long setConfig)
-				: website(setWebsite),
+				: module(setModule),
+				  website(setWebsite),
 				  urlList(setUrlList),
 				  config(setConfig) {}
+
+		ThreadOptions( // copy thread options and set module
+				const std::string& setModule,
+				const ThreadOptions& options
+				) : module(setModule),
+					website(options.website),
+					urlList(options.urlList),
+					config(options.config) {}
 	};
 
 } /* crawlservpp::Struct */
