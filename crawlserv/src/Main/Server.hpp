bb4152c484ebf8ecbe5586fb26ec0357c6bd8450
Ans <ans@ohai.su>
2019-04-30 13:35:09 +0300

    directories

--- crawlserv/src/Main/Server.hpp
+++ crawlserv/src/Main/Server.hpp
@@ -13,8 +13,10 @@
 #ifndef MAIN_SERVER_HPP_
 #define MAIN_SERVER_HPP_
 
-// hard-coded constant for server-side sleep on mySQL error
-#define MAIN_SERVER_SLEEP_ON_SQL_ERROR_SEC 5
+// hard-coded constants
+#define MAIN_SERVER_DIR_CACHE "cache" // directory for file cache
+#define MAIN_SERVER_DIR_COOKIES "cookies" // directory for cookies
+#define MAIN_SERVER_SLEEP_ON_SQL_ERROR_SEC 5 // server-side sleep on mySQL error
 
 #include "Database.hpp"
 #include "WebServer.hpp"
