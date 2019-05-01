6e3083ed548608d3033d70f26e89580c31576624
Ans <ans@ohai.su>
2019-05-02 01:24:23 +0300

    to be extended

--- crawlserv_frontend/crawlserv/php/data.php
+++ crawlserv_frontend/crawlserv/php/data.php
@@ -10,25 +10,40 @@ else
 
 if(isset($_POST["website"]))
     $website = $_POST["website"];
+
 ?>
+
 <h2>Import/Export</h2>
+
 <div class="content-block">
 <div class="entry-row">
+
 <div class="entry-label">Action:</div><div class="entry-input">
+
 <input type="radio" name="action" value="import"<?php
+
 if($action == "import")
     echo " checked";
+
 ?> /> Import &emsp;
+
 <input type="radio" name="action" value="merge"<?php
+
 if($action == "merge")
     echo " checked";
+
 ?> /> Merge &emsp;
+
 <input type="radio" name="action" value="export"<?php
+
 if($action == "export")
     echo " checked";
+
 ?> /> Export
+
 </div>
 </div>
+
 <div class="entry-row">
 
 </div>
