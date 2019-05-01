06b2b6445cc4aceb12b92f6be9407850cda9efe0
Ans <ans@ohai.su>
2019-05-01 20:17:44 +0300

    coding style

--- crawlserv_frontend/crawlserv/php/parsers.php
+++ crawlserv_frontend/crawlserv/php/parsers.php
@@ -1,43 +1,74 @@
 <?php
-if(isset($_POST["website"])) $website = $_POST["website"];
-if(isset($_POST["config"])) $config = $_POST["config"];
-if(isset($_POST["mode"])) $mode = $_POST["mode"];
-else $mode = "simple";
-if(isset($_POST["current"])) $current = $_POST["current"];
+if(isset($_POST["website"]))
+    $website = $_POST["website"];
+
+if(isset($_POST["config"]))
+    $config = $_POST["config"];
+
+if(isset($_POST["mode"]))
+    $mode = $_POST["mode"];
+else
+    $mode = "simple";
+
+if(isset($_POST["current"]))
+    $current = $_POST["current"];
 ?>
 <h2>Parsing options<?php 
 echo "<span id=\"opt-mode\">\n";
-if($mode == "simple") echo "<b>simple</b>";
-else echo "<a href=\"#\" class=\"action-link post-redirect-mode\" data-m=\"$m\" data-mode=\"simple\">simple</a>\n";
+
+if($mode == "simple")
+    echo "<b>simple</b>";
+else
+    echo "<a href=\"#\" class=\"action-link post-redirect-mode\" data-m=\"$m\" data-mode=\"simple\">simple</a>\n";
+
 echo " &middot; ";
-if($mode == "advanced") echo "<b>advanced</b>";
-else echo "<a href=\"#\" class=\"action-link post-redirect-mode\" data-m=\"$m\" data-mode=\"advanced\">advanced</a>\n";
+
+if($mode == "advanced")
+    echo "<b>advanced</b>";
+else
+    echo "<a href=\"#\" class=\"action-link post-redirect-mode\" data-m=\"$m\" data-mode=\"advanced\">advanced</a>\n";
+
 echo "</span>\n";
 ?></h2>
 <div class="content-block">
 <div class="entry-row">
 <div class="entry-label">Website:</div><div class="entry-input">
-<select class="entry-input" id="website-select" data-m="<?php echo $m; ?>" data-mode="<?php echo $mode; ?>">
+<select class="entry-input" id="website-select" data-m="<?php 
+echo $m;
+?>" data-mode="<?php 
+echo $mode; 
+?>">
 <?php
 $result = $dbConnection->query("SELECT id,name FROM crawlserv_websites ORDER BY name");
-if(!$result) exit("ERROR: Could not get ids and names of websites.");
+
+if(!$result)
+    exit("ERROR: Could not get ids and names of websites.");
+
 $first = true;
+
 while($row = $result->fetch_assoc()) {
     $id = $row["id"];
     $name = $row["name"];
     
     if($first) {
-        if(!isset($website)) $website = $id;
+        if(!isset($website))
+            $website = $id;
+        
         $first = false;
     }
+    
     echo "<option value=\"".$id."\"";
-    if($website == $id) {
+    
+    if($website == $id)
         echo " selected";
-    }
+    
     echo ">".htmlspecialchars($name)."</option>\n";
 }
+
 $result->close();
-if(!isset($website)) echo "<option disabled>No websites available</option>\n";
+
+if(!isset($website))
+    echo "<option disabled>No websites available</option>\n";
 ?>
 </select>
 </div>
@@ -48,48 +79,75 @@ if(isset($website)) {
     echo "<div class=\"entry-label\">Parser:</div><div class=\"entry-x-input\">\n";
     echo "<select id=\"config-select\" class=\"entry-x-input\" data-m=\"$m\" data-mode=\"$mode\">\n";
     
-    $result = $dbConnection->query("SELECT id, name FROM crawlserv_configs WHERE website=$website AND module='parser' ORDER BY name");
-    if(!$result) exit("ERROR: Could not get ids and names of URL lists.");
+    $result = $dbConnection->query(
+            "SELECT id, name FROM crawlserv_configs WHERE website=$website AND module='parser' ORDER BY name"
+    );
+    
+    if(!$result)
+        exit("ERROR: Could not get ids and names of URL lists.");
+    
     $first = true;
+    
     while($row = $result->fetch_assoc()) {
         $id = $row["id"];
         $name = $row["name"];
         
         if($first) {
-            if(!isset($config)) $config = $id;
+            if(!isset($config))
+                $config = $id;
+            
             $first = false;
         }
+        
         echo "<option value=\"".$id."\"";
         
-        if($id == $config) echo " selected";
+        if($id == $config)
+            echo " selected";
         
         echo ">$name</option>\n";
         
     }
+    
     $result->close();
-    if(!isset($config)) $config = 0;
+    
+    if(!isset($config))
+        $config = 0;
     
     if($config) {
         $result = $dbConnection->query("SELECT name, config FROM crawlserv_configs WHERE id=$config LIMIT 1");
-        if(!$result) exit("ERROR: Could not get configuration data.");
+        
+        if(!$result)
+            exit("ERROR: Could not get configuration data.");
+        
         $row = $result->fetch_assoc();
+        
         $configName = $row["name"];
-        if(!isset($current)) $current = $row["config"];
+        
+        if(!isset($current))
+            $current = $row["config"];
+        
         $result->close();
     }
-    else if(!isset($current)) $current = '[]';
+    else if(!isset($current))
+        $current = '[]';
     
     echo "<option value=\"0\"";
-    if(!$config) echo " selected";
+    
+    if(!$config)
+        echo " selected";
+    
     echo ">Add new</option>\n";
     
-    echo "</select>\n<a href=\"#\" class=\"actionlink config-delete\" data-m=\"$m\"><span class=\"remove-entry\">X</span></a></div>\n</div>\n";
+    echo "</select>\n";
+    echo "<a href=\"#\" class=\"actionlink config-delete\" data-m=\"$m\">";
+    echo "<span class=\"remove-entry\">X</span></a></div>\n</div>\n";
 }
 ?>
 <div class="action-link-box">
 <div class="action-link">
 <?php
-if($config) echo "<a href=\"#\" class=\"action-link config-duplicate\" data-m=\"$m\" data-mode=\"$mode\">Duplicate configuration</a>\n";
+if($config)
+    echo "<a href=\"#\" class=\"action-link config-duplicate\" data-m=\"$m\" data-mode=\"$mode\">Duplicate configuration</a>\n";
 ?>
 </div>
 </div>
@@ -103,13 +161,18 @@ if(isset($website)) {
         ." data-tippy-delay=\"0\" data-tippy-duration=\"0\" data-tippy-arrow=\"true\" data-tippy-placement=\"left-start\""
         ." data-tippy-size=\"small\">\n";
     echo "<input type=\"text\" class=\"opt\" id=\"config-name\" value=\"";
-    if(isset($_POST["name"])) echo $_POST["name"];
-    else if($config) echo $configName;
+    
+    if(isset($_POST["name"]))
+        echo $_POST["name"];
+    else if($config)
+        echo $configName;
+    
     echo "\" />\n";
     echo "</div>\n";
     echo "</div>\n";
     echo "<div class=\"action-link-box\">\n";
     echo "<div class=\"action-link\">\n";
+    
     if($config) {
         echo "<a href=\"#\" class=\"action-link config-update\" data-m=\"$m\" data-mode=\"$mode\">";
         echo "Change parser</a>\n";
@@ -118,6 +181,7 @@ if(isset($website)) {
         echo "<a href=\"#\" class=\"action-link config-add\" data-module=\"parser\" data-m=\"$m\" data-mode=\"$mode\">";
         echo "Add parser</a>\n";
     }
+    
     echo "</div>\n";
     echo "</div>\n";
     echo "<div id=\"config-view\"></div>\n";
@@ -141,34 +205,56 @@ if(isset($website)) {
 <?php 
 if($website) {
     echo "var db_locales = [";
+    
     $result = $dbConnection->query("SELECT name FROM crawlserv_locales ORDER BY name");
-    if(!$result) exit("ERROR: Could not get locales from database.");
+    
+    if(!$result)
+        exit("ERROR: Could not get locales from database.");
+    
     $locales = "";
-    while($row = $result->fetch_assoc()) $locales .= " \"".$row["name"]."\",";
+    
+    while($row = $result->fetch_assoc())
+        $locales .= " \"".$row["name"]."\",";
+    
     $locales = substr($locales, 0, -1);
+    
     echo "$locales ];\n";
     
     echo "var db_queries = [\n";
-    $result = $dbConnection->query("SELECT id,name,type,resultbool,resultsingle,resultmulti FROM crawlserv_queries WHERE website="
-        .$website." OR website IS NULL ORDER BY name");
-    if(!$result) exit("ERROR: Could not get ids and names of queries from database.");
-    while($row = $result->fetch_assoc()) {
+    
+    $result = $dbConnection->query(
+            "SELECT id,name,type,resultbool,resultsingle,resultmulti FROM crawlserv_queries WHERE website="
+            .$website." OR website IS NULL ORDER BY name"
+    );
+    
+    if(!$result)
+        exit("ERROR: Could not get ids and names of queries from database.");
+    
+    while($row = $result->fetch_assoc())
         echo " { \"id\": ".$row["id"].", \"name\": \"".$row["name"]."\", \"type\": \"".$row["type"]."\", \"resultbool\": "
             .($row["resultbool"] ? "true" : "false").", \"resultsingle\": ".($row["resultsingle"] ? "true" : "false")
             .", \"resultmulti\": ".($row["resultmulti"] ? "true" : "false")." },\n";
-    }
+    
     $result->close();
+    
     echo "];\n";
     
     echo "var db_config = \n";
+    
     if($config) {
         $result = $dbConnection->query("SELECT config FROM crawlserv_configs WHERE id=$config LIMIT 1");
-        if(!$result) exit("ERROR: Could not get current config from database");
+        
+        if(!$result)
+            exit("ERROR: Could not get current config from database");
+        
         $row = $result->fetch_assoc();
+        
         echo " ".$row["config"];
+        
         $result->close();
     }
-    else echo " []\n";
+    else
+        echo " []\n";
     
     echo ";\n";
 }
