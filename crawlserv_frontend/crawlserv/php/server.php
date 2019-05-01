3958a42343b734a65fea2fd8bfc93af899b91b40
Ans <ans@ohai.su>
2019-05-02 01:21:22 +0300

    coding style

--- crawlserv_frontend/crawlserv/php/server.php
+++ crawlserv_frontend/crawlserv/php/server.php
@@ -1,30 +1,53 @@
+
 <h2>Status</h2>
+
 <div class="content-block">
+
 <div id="server-status">
+
 <div class="checking-server-status">checking status...</div>
+
 </div> <span id="server-ping-value"></span><span id="server-ping"></span>
+
 </div>
+
 <h2>Control</h2>
+
 <div class="content-block">
 <div class="entry-row">
+
 <div class="entry-label">IP address:</div><div class="entry-input">
+
 <input id="cmd-allow-form-ip" class="entry-input" type="text" /></div>
+
 </div>
+
 <div class="action-link-box">
+
 <div class="action-link"><a href="#" class="action-link cmd-allow">Allow access to server</a></div>
 <div class="action-link"><a href="#" class="action-link cmd" data-cmd="disallow">Reset allowed IP addresses</a></div>
+
 </div>
 </div>
+
 <div class="content-block">
 <div class="entry-row">
+
 <div class="entry-label">Command:</div><div class="entry-input">
+
 <input id="cmd-custom-form-cmd" class="entry-input" type="text" /></div>
+
 </div>
+
 <div id="cmd-args"></div>
+
 <div class="action-link-box">
+
 <div class="action-link"><a href="#" class="action-link cmd-custom-addarg">Add argument</a></div>
 <div class="action-link"><a href="#" class="action-link cmd-custom">Run command</a></div>
+
 </div>
 </div>
+
 <div class="content-block"><a href="#" class="cmd kill" data-cmd="kill"><span id="kill-server">Kill server</span></a></div>
 
