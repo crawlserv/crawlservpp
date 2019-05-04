d3d9406adc85c6dbbc7ee59c1fc71e9481db8a57
Ans <ans@ohai.su>
2019-05-04 23:36:53 +0300

    exception handling

--- crawlserv/src/Helper/System.hpp
+++ crawlserv/src/Helper/System.hpp
@@ -7,22 +7,23 @@
  *      Author: ans
  */
 
+#include "../Main/Exception.hpp"
+
 #include <array>
 #include <cstdio>
 #include <iostream>
 #include <memory>
-#include <stdexcept>
 #include <string>
 
 namespace crawlservpp::Helper::System {
-	// execute a system command and return the stdout of the program, throws std::runtime_error
+	// execute a system command and return the stdout of the program, throws Main::Exception
 	inline std::string exec(const char* cmd) {
 	    std::array<char, 128> buffer;
 	    std::string result;
 	    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
 
 	    if(!pipe)
-	        throw std::runtime_error("popen() failed");
+	        throw Main::Exception("popen() failed");
 
 	    while(fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
 	        result += buffer.data();
