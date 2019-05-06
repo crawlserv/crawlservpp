bbbc6fbb32cc0401d420d01be30e3ca7ecce4fdc
Ans <ans@ohai.su>
2019-05-06 16:14:46 +0300

    coding style

--- crawlserv/src/Helper/Portability/getch.h
+++ crawlserv/src/Helper/Portability/getch.h
@@ -30,13 +30,20 @@ namespace crawlservpp::Helper::Portability {
 	inline char getch() {
 #ifdef __unix
 		char ch = 0;
+
 		tcgetattr(0, &oldT);
+
 		newT = oldT;
+
 		newT.c_lflag &= ~ICANON;
 		newT.c_lflag &= ~ECHO;
+
 		tcsetattr(0, TCSANOW, &newT);
+
 		ch = getchar();
+
 		tcsetattr(0, TCSANOW, &oldT);
+
 		return ch;
 #else
 		return ::getch();
