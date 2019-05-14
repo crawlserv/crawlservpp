f92e5a8856f7b1850f6f2ba77dbe5ca71ee05d82
Ans <ans@ohai.su>
2019-05-14 11:46:16 +0300

    use std::pair constructor, std::find_if

--- crawlserv/src/Main/ConfigFile.cpp
+++ crawlserv/src/Main/ConfigFile.cpp
@@ -18,19 +18,18 @@ namespace crawlservpp::Main {
 
 		if(fileStream.is_open()) {
 			while(std::getline(fileStream, line)) {
-				std::pair<std::string, std::string> entry;
 				unsigned long nameEnd = line.find('=');
 
-				if(nameEnd < line.length()) {
-					entry.first = boost::algorithm::to_lower_copy(line.substr(0, nameEnd));
-					entry.second = line.substr(nameEnd + 1);
-				}
-				else {
-					entry.first = line;
-					entry.second = "";
-				}
-
-				this->entries.emplace_back(entry);
+				if(nameEnd < line.length())
+					this->entries.emplace_back(
+							boost::algorithm::to_lower_copy(line.substr(0, nameEnd)),
+							line.substr(nameEnd + 1)
+					);
+				else
+					this->entries.emplace_back(
+							line,
+							""
+					);
 			}
 
 			fileStream.close();
@@ -41,11 +40,16 @@ namespace crawlservpp::Main {
 
 	// get value of config entry (or empty string if entry does not exist)
 	std::string ConfigFile::getValue(const std::string& name) const {
-		for(auto i = entries.begin(); i != entries.end(); ++i) {
-			if(i->first == boost::algorithm::to_lower_copy(name)) {
-				return i->second;
-			}
-		}
+		const auto valueIt = std::find_if(
+				this->entries.begin(),
+				this->entries.end(),
+				[&name](const auto& entry) {
+					return entry.first == boost::algorithm::to_lower_copy(name);
+				}
+		);
+
+		if(valueIt != this->entries.end())
+			return valueIt->second;
 
 		return "";
 	}
