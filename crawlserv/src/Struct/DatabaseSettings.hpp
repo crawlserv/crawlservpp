de7390254c3b2e30c129ff03f688e525ed4a352f
Ans <ans@ohai.su>
2019-05-14 22:25:37 +0300

    commenting, coding style

--- crawlserv/src/Struct/DatabaseSettings.hpp
+++ crawlserv/src/Struct/DatabaseSettings.hpp
@@ -10,23 +10,33 @@
 #ifndef STRUCT_DATABASESETTINGS_HPP_
 #define STRUCT_DATABASESETTINGS_HPP_
 
-#include <string>
+#include <string>	// std::string
 
 namespace crawlservpp::Struct {
 
 	struct DatabaseSettings {
-		std::string host; // host name of database server
-		unsigned short port; // port of database server
-		std::string user; // user name for database
-		std::string password; // user password for database
-		std::string name; // name/schema of database
-		bool compression; // use compression protocol
+		std::string host;		// host name of database server
+		unsigned short port;	// port of database server
+		std::string user;		// user name for database
+		std::string password;	// user password for database
+		std::string name;		// name/schema of database
+		bool compression;		// use compression protocol
 
 		// constructors
 		DatabaseSettings() : port(0), compression(false) {}
-		DatabaseSettings(const std::string& setHost, unsigned short setPort, const std::string& setUser,
-				const std::string& setPassword, const std::string& setName, bool setCompression)
-				: host(setHost), port(setPort), user(setUser), password(setPassword), name(setName), compression(setCompression) {}
+		DatabaseSettings(
+				const std::string& setHost,
+				unsigned short setPort,
+				const std::string& setUser,
+				const std::string& setPassword,
+				const std::string& setName,
+				bool setCompression)
+		: host(setHost),
+		  port(setPort),
+		  user(setUser),
+		  password(setPassword),
+		  name(setName),
+		  compression(setCompression) {}
 	};
 
 } /* crawlservpp::Struct */
