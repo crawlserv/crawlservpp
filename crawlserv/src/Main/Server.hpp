9e728921dab238e5aa2c6cd696c4d6cd9da0f048
Ans <ans@ohai.su>
2019-08-02 00:42:28 +0200

    minor formatting

--- crawlserv/src/Main/Server.hpp
+++ crawlserv/src/Main/Server.hpp
@@ -35,9 +35,9 @@
 #define MAIN_SERVER_HPP_
 
 // hard-coded constants
-#define MAIN_SERVER_DIR_CACHE "cache" // directory for file cache
-#define MAIN_SERVER_DIR_COOKIES "cookies" // directory for cookies
-#define MAIN_SERVER_SLEEP_ON_SQL_ERROR_SEC 5 // server-side sleep on mySQL error
+#define MAIN_SERVER_DIR_CACHE "cache"			// directory for file cache
+#define MAIN_SERVER_DIR_COOKIES "cookies"		// directory for cookies
+#define MAIN_SERVER_SLEEP_ON_SQL_ERROR_SEC 5	// server-side sleep on mySQL error
 
 // macros for exception handling of worker threads
 #define MAIN_SERVER_WORKER_BEGIN	try {
