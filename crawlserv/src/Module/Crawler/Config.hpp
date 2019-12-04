73f90b0023bade799620b160dfba1a4fee0d4534
Ans <ans@ohai.su>
2019-12-04 15:32:28 +0100

    add HTTP codes to be treated as connection errors (server down, timeout)

--- crawlserv/src/Module/Crawler/Config.hpp
+++ crawlserv/src/Module/Crawler/Config.hpp
@@ -213,10 +213,14 @@ namespace crawlservpp::Module::Crawler {
 		this->crawlerArchivesNames.emplace_back("archives.org");
 		this->crawlerArchivesUrlsMemento.emplace_back("http://web.archive.org/web/");
 		this->crawlerArchivesUrlsTimemap.emplace_back("http://web.archive.org/web/timemap/link/");
+
 		this->crawlerRetryHttp.push_back(429);
 		this->crawlerRetryHttp.push_back(502);
 		this->crawlerRetryHttp.push_back(503);
 		this->crawlerRetryHttp.push_back(504);
+		this->crawlerRetryHttp.push_back(521);
+		this->crawlerRetryHttp.push_back(522);
+		this->crawlerRetryHttp.push_back(524);
 	}
 
 	// parse crawling-specific configuration option
