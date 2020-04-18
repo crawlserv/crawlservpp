301942155964da83d817235b148d5abf8c53a679
Ans <ans@ohai.su>
2020-04-18 02:17:36 +0200

    2020

--- crawlserv/src/Data/Compression/Gzip.hpp
+++ crawlserv/src/Data/Compression/Gzip.hpp
@@ -1,7 +1,8 @@
 /*
+ *
  * ---
  *
- *  Copyright (C) 2019 Anselm Schmidt (ans[ät]ohai.su)
+ *  Copyright (C) 2020 Anselm Schmidt (ans[ät]ohai.su)
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
