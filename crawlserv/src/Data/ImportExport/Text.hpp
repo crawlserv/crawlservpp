3b06516af7cb0e950902e981fe7237dc678f7eef
Ans <ans@ohai.su>
2020-04-18 02:18:35 +0200

    2020

--- crawlserv/src/Data/ImportExport/Text.hpp
+++ crawlserv/src/Data/ImportExport/Text.hpp
@@ -2,7 +2,7 @@
  *
  * ---
  *
- *  Copyright (C) 2019 Anselm Schmidt (ans[ät]ohai.su)
+ *  Copyright (C) 2020 Anselm Schmidt (ans[ät]ohai.su)
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -20,7 +20,7 @@
  *
  * ---
  *
- * Text.h
+ * Text.hpp
  *
  * Namespace with functions to import/export data from/to text file.
  *
