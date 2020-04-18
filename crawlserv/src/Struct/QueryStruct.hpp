a920cad53c1eac6fc695a891f983e9cd371012a8
Ans <ans@ohai.su>
2020-04-18 07:17:39 +0200

    std::size_t, std::uint8_t, constructor for initializing values

--- crawlserv/src/Struct/QueryStruct.hpp
+++ crawlserv/src/Struct/QueryStruct.hpp
@@ -2,7 +2,7 @@
  *
  * ---
  *
- *  Copyright (C) 2019 Anselm Schmidt (ans[ät]ohai.su)
+ *  Copyright (C) 2020 Anselm Schmidt (ans[ät]ohai.su)
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -31,39 +31,58 @@
 #ifndef STRUCT_QUERYSTRUCT_HPP_
 #define STRUCT_QUERYSTRUCT_HPP_
 
+#include <cstddef>	// std::size_t
+#include <cstdint>	// std::uint8_t
+
 namespace crawlservpp::Struct {
 
 	struct QueryStruct {
-		static const unsigned char typeNone = 0;
-		static const unsigned char typeRegEx = 1;
-		static const unsigned char typeXPath = 2;
-		static const unsigned char typeJsonPointer = 3;
-		static const unsigned char typeJsonPath = 4;
-		static const unsigned char typeXPathJsonPointer = 5;
-		static const unsigned char typeXPathJsonPath = 6;
+		static const std::uint8_t typeNone = 0;
+		static const std::uint8_t typeRegEx = 1;
+		static const std::uint8_t typeXPath = 2;
+		static const std::uint8_t typeJsonPointer = 3;
+		static const std::uint8_t typeJsonPath = 4;
+		static const std::uint8_t typeXPathJsonPointer = 5;
+		static const std::uint8_t typeXPathJsonPath = 6;
 
-		unsigned char type;
-		size_t index;
+		std::uint8_t type;
+		std::size_t index;
 		bool resultBool;
 		bool resultSingle;
 		bool resultMulti;
 		bool resultSubSets;
 
-		// constructor: set default values
-		QueryStruct() {
-			this->type = QueryStruct::typeNone;
-			this->index = 0;
-			this->resultBool = false;
-			this->resultSingle = false;
-			this->resultMulti = false;
-			this->resultSubSets = false;
-		}
+		// constructor A: initialize values
+		QueryStruct(
+				std::uint8_t type,
+				std::size_t index,
+				bool resultBool,
+				bool resultSingle,
+				bool resultMulti,
+				bool resultSubSets
+		) : 		type(type),
+					index(index),
+					resultBool(resultBool),
+					resultSingle(resultSingle),
+					resultMulti(resultMulti),
+					resultSubSets(resultSubSets) {}
 
-		// boolean operators
+		// constructor B: set default values via constructor A
+		QueryStruct() : QueryStruct(
+				QueryStruct::typeNone,
+				0,
+				false,
+				false,
+				false,
+				false
+		) {}
+
+		// boolean operator
 		explicit operator bool() const noexcept {
 			return this->index != 0;
 		}
 
+		// inverse boolean operator
 		bool operator!() const noexcept {
 			return this->index == 0;
 		}
