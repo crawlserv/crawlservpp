967549fdf8b6a39f4c8c61096baa4e5390e8ba67
Ans <ans@ohai.su>
2020-04-18 07:56:11 +0200

    2020

--- crawlserv_frontend/crawlserv/php/statistics.php
+++ crawlserv_frontend/crawlserv/php/statistics.php
@@ -5,7 +5,7 @@
  * 
  * ---
  *
- *  Copyright (C) 2019 Anselm Schmidt (ans[ät]ohai.su)
+ *  Copyright (C) 2020 Anselm Schmidt (ans[ät]ohai.su)
  * 
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
