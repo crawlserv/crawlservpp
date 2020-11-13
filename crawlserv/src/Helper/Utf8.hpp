7e8a61b023b86946da10220e129e6453b24b8769
Ans <ans@ohai.su>
2020-11-13 20:35:58 +0200

    UTF-8 function for number of codepoints

--- crawlserv/src/Helper/Utf8.hpp
+++ crawlserv/src/Helper/Utf8.hpp
@@ -35,6 +35,7 @@
 
 #include "../_extern/utf8/source/utf8.h"
 
+#include <cstddef>		// std::size_t
 #include <string>		// std::string
 #include <string_view>	// std::string_view
 
@@ -101,6 +102,12 @@ namespace crawlservpp::Helper::Utf8 {
 	bool repairUtf8(std::string_view strIn, std::string& strOut);
 
 	///@}
+	///@name Length
+	///@{
+
+	std::size_t length(std::string_view str);
+
+	///@}
 
 	/*
 	 * EXCEPTION CLASS
@@ -116,6 +123,10 @@ namespace crawlservpp::Helper::Utf8 {
 
 	/*
 	 * IMPLEMENTATION
+	 */
+
+	/*
+	 * CONVERSION
 	 */
 
 	//! Converts a string from ISO-8859-1 to UTF-8.
@@ -143,6 +154,10 @@ namespace crawlservpp::Helper::Utf8 {
 
 		return strOut;
 	}
+
+	/*
+	 * VALIDATION
+	 */
 
 	//! Checks whether a string contains valid UTF-8.
 	/*!
@@ -249,6 +264,10 @@ namespace crawlservpp::Helper::Utf8 {
 		return utf8::distance(stringToCheck.begin(), stringToCheck.end()) == 1;
 	}
 
+	/*
+	 * REPAIR
+	 */
+
 	//! Replaces invalid UTF-8 characters in the given string and returns whether invalid characters occured.
 	/*!
 	 * Uses the @c UTF8-CPP library for UTF-8
@@ -285,6 +304,79 @@ namespace crawlservpp::Helper::Utf8 {
 		catch(const utf8::exception& e) {
 			throw Exception("UTF-8 error: " + std::string(e.what()));
 		}
+	}
+
+	/*
+	 * LENGTH
+	 */
+
+	/*!
+	 * Returns the number of UTF-8 codepoints in the given string.
+	 *
+	 * \param str The string to be checked
+	 *   for UTF-8 codepoints.
+	 *
+	 * \returns The number of UTF-8
+	 *   codepoints found in the
+	 *   string.
+	 *
+	 * \throws Utf8::Exception if the
+	 *   string contains invalid
+	 *   UTF-8 codepoints.
+	 */
+	inline std::size_t length(std::string_view str) {
+		constexpr unsigned char maxOneByte{127};
+		constexpr unsigned char checkTwoBytes{0xE0};
+		constexpr unsigned char isTwoBytes{0xC0};
+		constexpr unsigned char checkThreeBytes{0xF0};
+		constexpr unsigned char isThreeBytes{0xE0};
+		constexpr unsigned char checkFourBytes{0xF8};
+		constexpr unsigned char isFourBytes{0xF0};
+
+		constexpr std::size_t skipTwoBytes{2};
+		constexpr std::size_t skipThreeBytes{3};
+
+		std::size_t result{0};
+
+		const auto bytes{
+			str.length()
+		};
+
+		for(std::size_t pos{0}; pos < bytes; ++pos) {
+			++result;
+
+			const unsigned char byte{
+				static_cast<unsigned char>(str[pos])
+			};
+
+			if(byte <= maxOneByte) {
+				// one byte
+				continue;
+			}
+
+			if((byte & checkTwoBytes) == isTwoBytes) {
+				// two bytes
+				++pos;
+			}
+			else if((byte & checkThreeBytes) == isThreeBytes) {
+				// three bytes
+				pos += skipTwoBytes;
+			}
+			else if((byte & checkFourBytes) == isFourBytes) {
+				// four bytes
+				pos += skipThreeBytes;
+			}
+			else {
+				std::string exceptionString{"Invalid UTF-8 in '"};
+
+				exceptionString += str;
+				exceptionString += "'";
+
+				throw Exception(exceptionString);
+			}
+		}
+
+		return result;
 	}
 
 } /* namespace crawlservpp::Helper::Utf8 */
