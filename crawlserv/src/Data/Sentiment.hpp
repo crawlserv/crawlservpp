cdd61808abe3d72c576c1906427be3e74e2944b2
Ans <ans@ohai.su>
2020-12-31 01:01:22 +0200

    commenting

--- crawlserv/src/Data/Sentiment.hpp
+++ crawlserv/src/Data/Sentiment.hpp
@@ -28,9 +28,9 @@
  *
  * If you use the VADER sentiment analysis tools, please cite:
  *
- * Hutto, C.J. & Gilbert, E.E. (2014). VADER: A Parsimonious Rule-based Model for
- * Sentiment Analysis of Social Media Text. Eighth International Conference on
- * Weblogs and Social Media (ICWSM-14). Ann Arbor, MI, June 2014.
+ *  Hutto, C.J. & Gilbert, E.E. (2014). VADER: A Parsimonious Rule-based Model for
+ *  Sentiment Analysis of Social Media Text. Eighth International Conference on
+ *  Weblogs and Social Media (ICWSM-14). Ann Arbor, MI, June 2014.
  *
  * !!! FOR ENGLISH LANGUAGE ONLY !!!
  *
