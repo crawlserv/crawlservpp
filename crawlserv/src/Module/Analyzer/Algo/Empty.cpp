e2c68caa110e26ac2f5492b826540015df6df842
Ans <ans@ohai.su>
2021-02-05 19:11:31 +0200

    minor commenting

--- crawlserv/src/Module/Analyzer/Algo/Empty.cpp
+++ crawlserv/src/Module/Analyzer/Algo/Empty.cpp
@@ -86,7 +86,7 @@ namespace crawlservpp::Module::Analyzer::Algo {
 	//! Returns the name of the algorithm.
 	/*!
 	 * \returns A string view containing the
-		 *   name of the implemented algorithm.
+	 *   name of the implemented algorithm.
 	 */
 	std::string_view Empty::getName() const {
 		// TODO: Insert the name of the algorithm.
