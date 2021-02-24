crawlserv/crawlservpp

commit 932079bce7ce5b6ae51d2bff870e36061d6fad07

unify corpus manipulators

crawlserv/src/Module/Analyzer/Database.cpp
@@ -1233,34 +1233,26 @@ namespace crawlservpp::Module::Analyzer {
 				corpusStatement.setString(sqlArg2, properties.sourceTable);
 				corpusStatement.setString(sqlArg3, properties.sourceColumn);
 
-				if(
-						properties.wordManipulators.empty()
-						&& properties.sentenceManipulators.empty()
-				) {
+				if(properties.manipulators.empty()) {
 					corpusStatement.setNull(sqlArg4, 0);
 				}
 				else {
 					std::string lastSavePoint;
 
-					for(std::size_t n{}; n < properties.wordManipulators.size(); ++n) {
-						lastSavePoint += 'w';
-						lastSavePoint += std::to_string(properties.wordManipulators[n]);
+					for(std::size_t n{}; n < properties.manipulators.size(); ++n) {
+						lastSavePoint += std::to_string(properties.manipulators[n]);
 						lastSavePoint += '[';
 
-						if(properties.wordModels.size() > n) {
-							lastSavePoint += properties.wordModels[n];
+						if(properties.models.size() > n) {
+							lastSavePoint += properties.models[n];
 						}
 
-						lastSavePoint += ']';
-					}
+						if(properties.dictionaries.size() > n) {
+							lastSavePoint += properties.dictionaries[n];
+						}
 
-					for(std::size_t n{}; n < properties.sentenceManipulators.size(); ++n) {
-						lastSavePoint += 's';
-						lastSavePoint += std::to_string(properties.sentenceManipulators[n]);
-						lastSavePoint += '[';
-
-						if(properties.sentenceModels.size() > n) {
-							lastSavePoint += properties.sentenceModels[n];
+						if(properties.languages.size() > n) {
+							lastSavePoint += properties.languages[n];
 						}
 
 						lastSavePoint += ']';
@@ -1936,8 +1928,7 @@ namespace crawlservpp::Module::Analyzer {
 	) {
 		std::string savePoint;
 		std::string result;
-		std::size_t numSentenceManipulators{};
-		std::size_t numWordManipulators{};
+		std::size_t numManipulators{};
 
 		// check connection to database
 		this->checkConnection();
@@ -1962,21 +1953,23 @@ namespace crawlservpp::Module::Analyzer {
 			sqlStatement.setString(sqlArg4, corpusCreationTime);
 
 			for(
-					auto it{properties.sentenceManipulators.cbegin()};
-					it != properties.sentenceManipulators.cend();
+					auto it{properties.manipulators.cbegin()};
+					it != properties.manipulators.cend();
 					++it
 			) {
 				const auto index{
 					static_cast<std::size_t>(
 							it
-							- properties.sentenceManipulators.cbegin()
+							- properties.manipulators.cbegin()
 					)
 				};
 
 				savePoint += "s";
 				savePoint += std::to_string(*it);
 				savePoint += "[";
-				savePoint += properties.sentenceModels.at(index);
+				savePoint += properties.models.at(index);
+				savePoint += properties.dictionaries.at(index);
+				savePoint += properties.languages.at(index);
 				savePoint += "]";
 
 				// execute SQL query for
@@ -1991,42 +1984,7 @@ namespace crawlservpp::Module::Analyzer {
 					if(sqlResultSet->getBoolean("result")) {
 						result = savePoint;
 
-						numSentenceManipulators = index + 1;
-					}
-				}
-			}
-
-			for(
-					auto it{properties.wordManipulators.cbegin()};
-					it != properties.wordManipulators.cend();
-					++it
-			) {
-				const auto index{
-					static_cast<std::size_t>(
-							it
-							- properties.wordManipulators.cbegin()
-					)
-				};
-
-				savePoint += "w";
-				savePoint += std::to_string(*it);
-				savePoint += "[";
-				savePoint += properties.wordModels.at(index);
-				savePoint += "]";
-
-				// execute SQL query for
-				sqlStatement.setString(sqlArg5, savePoint);
-
-				SqlResultSetPtr sqlResultSet{
-					Database::sqlExecuteQuery(sqlStatement)
-				};
-
-				// get result
-				if(sqlResultSet && sqlResultSet->next()) {
-					if(sqlResultSet->getBoolean("result")) {
-						result = savePoint;
-
-						numWordManipulators = index + 1;
+						numManipulators = index + 1;
 					}
 				}
 			}
@@ -2036,18 +1994,10 @@ namespace crawlservpp::Module::Analyzer {
 		}
 
 		// remove manipulators that have already been run on save point
-		if(numWordManipulators > 0) {
-			properties.sentenceManipulators.clear();
-
-			properties.wordManipulators.erase(
-					properties.wordManipulators.begin(),
-					properties.wordManipulators.begin() + numWordManipulators
-			);
-		}
-		else if(numSentenceManipulators > 0) {
-			properties.sentenceManipulators.erase(
-					properties.sentenceManipulators.begin(),
-					properties.sentenceManipulators.begin() + numSentenceManipulators
+		if(numManipulators > 0) {
+			properties.manipulators.erase(
+					properties.manipulators.begin(),
+					properties.manipulators.begin() + numManipulators
 			);
 		}
 
@@ -2057,7 +2007,7 @@ namespace crawlservpp::Module::Analyzer {
 				std::upper_bound(
 						properties.savePoints.begin(),
 						properties.savePoints.end(),
-						numSentenceManipulators + numWordManipulators
+						numManipulators
 				)
 		);
 
@@ -2086,61 +2036,37 @@ namespace crawlservpp::Module::Analyzer {
 
 			if(
 					savePoint
-					> properties.sentenceManipulators.size()
-					+ properties.wordManipulators.size()
+					> properties.manipulators.size()
 			) {
 				continue;
 			}
 
-			std::vector<std::uint16_t> sentenceManipulators;
-			std::vector<std::string> sentenceModels;
-			std::vector<std::uint16_t> wordManipulators;
-			std::vector<std::string> wordModels;
+			std::vector<std::uint16_t> manipulators;
+			std::vector<std::string> models;
+			std::vector<std::string> dictionaries;
+			std::vector<std::string> languages;
+
+			Database::append(manipulators, properties.manipulators, done, savePoint);
+			Database::append(models, properties.models, done, savePoint);
+			Database::append(dictionaries, properties.dictionaries, done, savePoint);
+			Database::append(languages, properties.languages, done, savePoint);
 
 			for(auto manipulator{done}; manipulator < savePoint; ++manipulator) {
-				if(manipulator >= properties.sentenceManipulators.size()) {
-					// add word manipulator
-					const auto index{
-						manipulator - properties.sentenceManipulators.size()
-					};
-
-					wordManipulators.push_back(
-							properties.wordManipulators.at(index)
-					);
-					wordModels.push_back(
-							properties.wordModels.at(index)
-					);
-
-					savePointName += "w";
-					savePointName += std::to_string(wordManipulators.back());
-					savePointName += "[";
-					savePointName += wordModels.back();
-					savePointName += "]";
-				}
-				else {
-					// add sentence manipulator
-					sentenceManipulators.push_back(
-							properties.sentenceManipulators.at(manipulator)
-					);
-					sentenceModels.push_back(
-							properties.sentenceModels.at(manipulator)
-					);
-
-					savePointName += "s";
-					savePointName += std::to_string(sentenceManipulators.back());
-					savePointName += "[";
-					savePointName += sentenceModels.back();
-					savePointName += "]";
-				}
-
-				++done;
+				savePointName += std::to_string(manipulators.back());
+				savePointName += "[";
+				savePointName += models.at(manipulator);
+				savePointName += dictionaries.at(manipulator);
+				savePointName += languages.at(manipulator);
+				savePointName += "]";
 			}
 
+			done = savePoint;
+
 			if(!corpusRef.tokenize(
-					sentenceManipulators,
-					sentenceModels,
-					wordManipulators,
-					wordModels,
+					manipulators,
+					models,
+					dictionaries,
+					languages,
 					properties.freeMemoryEvery,
 					statusSetter
 			)) {
@@ -2160,49 +2086,24 @@ namespace crawlservpp::Module::Analyzer {
 		}
 
 		// run remaining manipulators, if necessary (result will not be saved to the database)
-		const auto total{
-			properties.sentenceManipulators.size()
-			+ properties.wordManipulators.size()
-		};
+		if(done < properties.manipulators.size()) {
+			std::vector<std::uint16_t> manipulators;
+			std::vector<std::string> models;
+			std::vector<std::string> dictionaries;
+			std::vector<std::string> languages;
 
-		if(done < total) {
-			std::vector<std::uint16_t> sentenceManipulators;
-			std::vector<std::string> sentenceModels;
-			std::vector<std::uint16_t> wordManipulators;
-			std::vector<std::string> wordModels;
-
-			for(auto manipulator{done}; manipulator < total; ++manipulator) {
-				if(manipulator >= properties.sentenceManipulators.size()) {
-					// add word manipulator
-					const auto index{
-						manipulator - properties.sentenceManipulators.size()
-					};
-
-					wordManipulators.push_back(
-							properties.wordManipulators.at(index)
-					);
-					wordModels.push_back(
-							properties.wordModels.at(index)
-					);
-				}
-				else {
-					// add sentence manipulator
-					sentenceManipulators.push_back(
-							properties.sentenceManipulators.at(manipulator)
-					);
-					sentenceModels.push_back(
-							properties.sentenceModels.at(manipulator)
-					);
-				}
-			}
+			Database::append(manipulators, properties.manipulators, done);
+			Database::append(models, properties.models, done);
+			Database::append(dictionaries, properties.dictionaries, done);
+			Database::append(languages, properties.languages, done);
 
 			statusSetter.change("Preprocessing corpus...");
 
 			return corpusRef.tokenize(
-					sentenceManipulators,
-					sentenceModels,
-					wordManipulators,
-					wordModels,
+					manipulators,
+					models,
+					dictionaries,
+					languages,
 					properties.freeMemoryEvery,
 					statusSetter
 			);
@@ -2210,7 +2111,7 @@ namespace crawlservpp::Module::Analyzer {
 
 		if(!corpusRef.isTokenized()) {
 			// tokenize without manipulators
-			return corpusRef.tokenizeCustom({}, {}, properties.freeMemoryEvery, statusSetter);
+			return corpusRef.tokenizeCustom({}, properties.freeMemoryEvery, statusSetter);
 		}
 
 		return statusSetter.isRunning();

crawlserv/src/Module/Analyzer/Config.hpp
@@ -204,45 +204,15 @@ namespace crawlservpp::Module::Analyzer {
 			///@name Corpus Tokenization
 			///@{
 
-			//! Steps after which the corpus will be stored in the database.
+			//! Dictionary for the (token-based) manipulator with the same array index.
 			/*!
-			 * If zero, the unmanipulated corpus will be
-			 *  stored. After that, the numbering starts
-			 *  with the sentence manipulators, and
-			 *  continues with the word manipulators.
+			 * Empty strings will be ignored.
 			 *
-			 * \note Savepoints will be ignored, if a
-			 *   suitable savepoint has been found beyond
-			 *   them.
+			 * Preprocessing of the corpus will
+			 *  fail, if no dictionary is set for
+			 *  a manipulator that requires one.
 			 */
-			std::vector<std::uint16_t> tokenizerSavePoints{};
-
-			//! Manipulators used on the sentences of the text corpus.
-			/*!
-			 * \sa Data::sentenceManipNone,
-			 * 	 Data::sentenceManipTagger,
-			 */
-			std::vector<std::uint16_t> tokenizerSentenceManipulators;
-
-			//! Model for the sentence manipulator with the same array index.
-			/*!
-			 * Empty strings will be ignored.
-			 */
-			std::vector<std::string> tokenizerSentenceModels;
-
-			//! Manipulators used on the words of the text corpus.
-			/*!
-			 * \sa Data::wordManipNone,
-			 *   Data::wordManipPorter2Stemmer,
-			 *   Data::wordManipGermanStemmer
-			 */
-			std::vector<std::uint16_t> tokenizerWordManipulators;
-
-			//! Model for the word manipulator with the same array index.
-			/*!
-			 * Empty strings will be ignored.
-			 */
-			std::vector<std::string> tokenizerWordModels;
+			std::vector<std::string> tokenizerDicts;
 
 			//! Number of processed bytes in a continuous corpus after which memory will be freed.
 			/*!
@@ -250,6 +220,51 @@ namespace crawlservpp::Module::Analyzer {
 			 *  processing is complete.
 			 */
 			std::uint64_t tokenizerFreeMemoryEvery{defaultFreeMemoryEvery};
+
+			//! Language for the (token-based aspell) manipulator with the same array index.
+			/*!
+			 * Empty strings will be ignored.
+			 *
+			 * If not set, the default language of the
+			 *  server's aspell configuration will be
+			 *  used.
+			 */
+			std::vector<std::string> tokenizerLanguages;
+
+			//! Manipulators used on the text corpus.
+			/*!
+			 * \sa Data::corpusManipNone,
+			 * 	 Data::corpusManipTagger,
+			 * 	 Data::corpusManipTaggerPosterior,
+			 * 	 Data::corpusManipEnglishStemmer,
+			 * 	 Data::corpusManipGermanStemmer,
+			 * 	 Data::corpusManipLemmatizer,
+			 * 	 Data::corpusManipRemove,
+			 * 	 Data::corpusManipCorrect
+			 */
+			std::vector<std::uint16_t> tokenizerManipulators;
+
+			//! Model for the (sentence-based) manipulator with the same array index.
+			/*!
+			 * Empty strings will be ignored.
+			 *
+			 * Preprocessing of the corpus will
+			 *  fail, if no model is set for a
+			 *  manipulator that requires one.
+			 */
+			std::vector<std::string> tokenizerModels;
+
+			//! Steps after which the corpus will be stored in the database.
+			/*!
+			 * If zero, the unmanipulated corpus will be
+			 *  stored. Starting from one, the number
+			 *  corresponds to the manipulators used.
+			 *
+			 * \note Savepoints will not be stored, if a
+			 *   suitable savepoint already exists beyond
+			 *   them.
+			 */
+			std::vector<std::uint16_t> tokenizerSavePoints{};
 
 			///@}
 		}
@@ -364,12 +379,12 @@ namespace crawlservpp::Module::Analyzer {
 
 		// corpus tokenization options
 		this->category("tokenizer");
-		this->option("savepoints", this->config.tokenizerSavePoints);
-		this->option("sentence.manipulators", this->config.tokenizerSentenceManipulators);
-		this->option("sentence models", this->config.tokenizerSentenceModels);
-		this->option("word.manipulators", this->config.tokenizerWordManipulators);
-		this->option("word.models", this->config.tokenizerWordModels);
+		this->option("dicts", this->config.tokenizerDicts);
 		this->option("free.memory.every", this->config.tokenizerFreeMemoryEvery);
+		this->option("languages", this->config.tokenizerLanguages);
+		this->option("manipulators", this->config.tokenizerManipulators);
+		this->option("models", this->config.tokenizerModels);
+		this->option("savepoints", this->config.tokenizerSavePoints);
 
 		// parse algo options
 		this->parseAlgoOption();
@@ -434,38 +449,51 @@ namespace crawlservpp::Module::Analyzer {
 			this->warning("Incomplete input field(s) removed from configuration.");
 		}
 
-		// check number of corpus manipulators and their models
+		// check number of manipulators and their dictionaries, models, and languages
 		if(
-				this->config.tokenizerSentenceModels.size()
-				> this->config.tokenizerSentenceManipulators.size()
+				this->config.tokenizerDicts.size()
+				> this->config.tokenizerManipulators.size()
 		) {
 			this->warning(
 					"The configuration contains"
-					" more sentence models than sentence"
-					" manipulators, redundant models will"
-					" be ignored."
+					" more dictionaries than manipulators,"
+					" redundant dictionaries will be ignored."
 			);
 		}
 
 		if(
-				this->config.tokenizerWordModels.size()
-				> this->config.tokenizerWordManipulators.size()
+				this->config.tokenizerModels.size()
+				> this->config.tokenizerManipulators.size()
 		) {
 			this->warning(
 					"The configuration contains"
-					" more word models than word"
-					" manipulators, redundant models will"
-					" be ignored."
+					" more models than manipulators,"
+					" redundant models will be ignored."
+			);
+		}
+
+		if(
+				this->config.tokenizerLanguages.size()
+				> this->config.tokenizerManipulators.size()
+		) {
+			this->warning(
+					"The configuration contains"
+					" more languages than manipulators,"
+					" redundant languages will be ignored."
 			);
 		}
 
 		// resize so that the numbers of models equals the numbers of manipulators
-		this->config.tokenizerSentenceModels.resize(
-				this->config.tokenizerSentenceManipulators.size()
+		this->config.tokenizerDicts.resize(
+				this->config.tokenizerManipulators.size()
 		);
 
-		this->config.tokenizerWordModels.resize(
-				this->config.tokenizerWordManipulators.size()
+		this->config.tokenizerModels.resize(
+				this->config.tokenizerManipulators.size()
+		);
+
+		this->config.tokenizerLanguages.resize(
+				this->config.tokenizerManipulators.size()
 		);
 
 		// check algo options

crawlserv/src/Module/Analyzer/Database.hpp
@@ -61,6 +61,8 @@
 #include <cstddef>			// std::size_t
 #include <cstdint>			// std::uint8_t, std::uint16_t, std::uint64_t
 #include <functional>		// std::function
+#include <iterator>			// std::begin, std::end
+#include <limits>			// std::numeric_limits
 #include <locale>			// std::locale
 #include <queue>			// std::queue
 #include <sstream>			// std::ostringstream
@@ -367,6 +369,21 @@ namespace crawlservpp::Module::Analyzer {
 				const std::string& savePoint,
 				StatusSetter& statusSetter
 		);
+
+		// internal helper function template
+		template<typename T> static void append(
+				T& to,
+				const T& from,
+				typename T::size_type startAt = 0,
+				typename T::size_type endAt = std::numeric_limits<typename T::size_type>::max()
+		) {
+			if(endAt == std::numeric_limits<typename T::size_type>::max()) {
+				to.insert(std::end(to), std::begin(from) + startAt, std::end(from));
+			}
+			else {
+				to.insert(std::end(to), std::begin(from) + startAt, std::begin(from) + endAt);
+			}
+		}
 	};
 
 } /* namespace crawlservpp::Module::Analyzer */

crawlserv/src/Module/Analyzer/Thread.cpp
@@ -488,10 +488,10 @@ namespace crawlservpp::Module::Analyzer {
 								this->config.generalInputSources.at(index),
 								this->config.generalInputTables.at(index),
 								this->config.generalInputFields.at(index),
-								this->config.tokenizerSentenceManipulators,
-								this->config.tokenizerSentenceModels,
-								this->config.tokenizerWordManipulators,
-								this->config.tokenizerWordModels,
+								this->config.tokenizerManipulators,
+								this->config.tokenizerModels,
+								this->config.tokenizerDicts,
+								this->config.tokenizerLanguages,
 								this->config.tokenizerSavePoints,
 								this->config.tokenizerFreeMemoryEvery
 						),

crawlserv/src/Struct/CorpusProperties.hpp
@@ -57,17 +57,17 @@ namespace crawlservpp::Struct {
 		//! The name of the table column from which the corpus is created.
 		std::string sourceColumn;
 
-		//! IDs of manipulators to change whole sentences.
-		std::vector<std::uint16_t> sentenceManipulators;
+		//! The IDs of manipulators for preprocessing the corpus.
+		std::vector<std::uint16_t> manipulators;
 
-		//! The models used by the sentence manipulators with the same array index.
-		std::vector<std::string> sentenceModels;
+		//! The models used by the manipulators with the same array index.
+		std::vector<std::string> models;
 
-		//! IDs of manipulators to change single words.
-		std::vector<std::uint16_t> wordManipulators;
+		//! The dictionaries used by the manipulators with the same array index.
+		std::vector<std::string> dictionaries;
 
-		//! The models used by the word manipulators with the same array index.
-		std::vector<std::string> wordModels;
+		//! The languages used by the manipulators with the same array index.
+		std::vector<std::string> languages;
 
 		//! List of savepoints.
 		/*!
@@ -114,16 +114,22 @@ namespace crawlservpp::Struct {
 		 * \param setSourceColumn Constant reference to a
 		 *   string containing the name of the table column
 		 *   from which the corpus is created.
-		 * \param setSentenceManipulators Constant reference
-		 *   to a vector containing the manipulators to be
-		 *   applied on each sentence of the corpus.
-		 * \param setSentenceModels Constant reference to a
-		 *   vector of strings, containing a model for each
-		 *   sentence manipulator, or an empty string if no
+		 * \param setManipulators Constant reference to a
+		 *   vector containing the manipulators to be
+		 *   applied when preprocessing the corpus.
+		 * \param setModels Constant reference to a vector
+		 *   of strings, containing a model for each
+		 *   manipulator, or an empty string if no
 		 *   model is required by the manipulator.
-		 * \param setWordManipulators Constant reference to
-		 *   a vector containing the manipulators to be
-		 *   applied on each word of the corpus.
+		 * \param setDictionaries Constant reference to a
+		 *   vector of strings, containing a dictionary for
+		 *   each manipulator, or an empty string if no
+		 *   dictionary is required by the manipulator.
+		 * \param setLanguages Constant reference to a
+		 *   vector of strings, containing a language for
+		 *   each manipulator, or an empty string if no
+		 *   language is required by the manipulator, or
+		 *   its default language should be used.
 		 * \param setWordModels Constant reference to a
 		 *   vector of strings, containing a model for each
 		 *   word manipulator, or an empty string if no
@@ -161,19 +167,19 @@ namespace crawlservpp::Struct {
 				std::uint16_t setSourceType,
 				const std::string& setSourceTable,
 				const std::string& setSourceColumn,
-				const std::vector<std::uint16_t>& setSentenceManipulators,
-				const std::vector<std::string>& setSentenceModels,
-				const std::vector<std::uint16_t>& setWordManipulators,
-				const std::vector<std::string>& setWordModels,
+				const std::vector<std::uint16_t>& setManipulators,
+				const std::vector<std::string>& setModels,
+				const std::vector<std::string>& setDictionaries,
+				const std::vector<std::string>& setLanguages,
 				const std::vector<std::uint16_t>& setSavePoints,
 				std::uint64_t setFreeMemoryEvery
 		) : sourceType{setSourceType},
 			sourceTable{setSourceTable},
 			sourceColumn{setSourceColumn},
-			sentenceManipulators{setSentenceManipulators},
-			sentenceModels{setSentenceModels},
-			wordManipulators{setWordManipulators},
-			wordModels{setWordModels},
+			manipulators{setManipulators},
+			models{setModels},
+			dictionaries{setDictionaries},
+			languages{setLanguages},
 			savePoints{setSavePoints},
 			freeMemoryEvery{setFreeMemoryEvery},
 			tokenize{true} {}

commit 1718a93e98c3d23c80039cf4531695745bb320c7

fixed bug (empty corpus instead of savepoint) introduced by 932079b

crawlserv/src/Module/Analyzer/Database.cpp
@@ -1964,7 +1964,6 @@ namespace crawlservpp::Module::Analyzer {
 					)
 				};
 
-				savePoint += "s";
 				savePoint += std::to_string(*it);
 				savePoint += "[";
 				savePoint += properties.models.at(index);